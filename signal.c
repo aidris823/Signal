2d9fac61b8d7ba75482f32604e23153ea6fc5141
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-11-09 01:16:52 -0500

    Done

--- signal.c
+++ signal.c
@@ -4,7 +4,19 @@
 #include <signal.h>
 #include <errno.h>
 
+static void sighandler(int signo){
+  if (signo == SIGINT){
+    printf("SIGINT Signal, emergency exit doors opened \n");
+    exit(0);
+  }
+  if (signo == SIGUSR1){
+    printf("Parent PID: %d\n",getppid());
+    //Dont' exit
+  }
+}
 int main(){
+  signal(SIGINT,sighandler);
+  signal(SIGUSR1,sighandler);
   while (1){
     printf("%d",getpid());
   }
